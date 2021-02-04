d79522dba801859af9ba8a3991c28894fb110068
eatea <al-loco@mail.ru>
2021-02-05 00:00:35 +0300

    CRUD_V1

---
+++ examples/crudV1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+// action
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to Home Page RESTAPI</h1>")
+	dump, _ := httputil.DumpRequest(r, true)
+	w.Write()
+
+	fmt.Fprintf(w, "<pre>")
+	fmt.Fprintf(w, "%q", dump)
+	fmt.Fprintf(w, "</pre>")
+	fmt.Println("Someone visited the HomePage...")
+}
+
+func EndServe() {
+	fmt.Println("---Stop server----")
+}
+
+func main() {
+	defer EndServe()
+	fmt.Println("----Start server----")
+	// set route
+	http.HandleFunc("/", HomePage)
+	// start serve
+	log.Fatal(http.ListenAndServe(":8050", nil))
+}
