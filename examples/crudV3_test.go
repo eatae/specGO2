2f9cdcd7a682e374fa98be359c3caabb44ba280b
eatea <al-loco@mail.ru>
2021-03-02 18:58:25 +0300

    crudV3_test

--- examples/crudV3_test.go
+++ examples/crudV3_test.go
@@ -6,18 +6,48 @@ import (
 	"testing"
 )
 
+/* тест для GET запроса без параметров */
 func TestShowArticles(t *testing.T) {
-	// запрос к нашему API
-	request, err := http.NewRequest("GET", "/articles", nil)
+	request, err := http.NewRequest("GET", "/articles", nil) // запрос к тестироваему API
 	if err != nil {
 		t.Fatal(err)
 	}
+	recorder := httptest.NewRecorder()			// рекордер
+	handler := http.HandlerFunc(ShowArticles)	// функция которую будем тестировать
+	handler.ServeHTTP(recorder, request)		// записываем в рекордер ответ API
+
+	/* check status code */
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	/* check answer (только если данные не динамичны) */
+	expectedAnswer := `[{"id":1,"item":"Wood chair","amount":8,"price":"12"},{"id":2,"item":"Red-wood table","amount":4,"price":"26"}]`
+	if recorder.Body.String() != expectedAnswer {
+		t.Error("wrong answer")
+	}
+}
+
+/* тест для GET запроса c параметрами */
+func TestShowArticleById(t *testing.T) {
+	request, err := http.NewRequest("GET", "/article", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	query := request.URL.Query()			// создаём параметры запроса
+	query.Add("id", "1")			// записываем значение параметра
+	request.URL.RawQuery = query.Encode()	// добавляем парметры к запросу
+
 	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(ShowArticles)
+	handler := http.HandlerFunc(ShowArticleById)
 	handler.ServeHTTP(recorder, request)
 
-	if status := rr.Code; status != http.StatusOK {
+	/* check status code */
+	if status := recorder.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 }
+
+
