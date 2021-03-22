290e3fee46b7c54f72404b17ba6010e095ed880c
Eatae <al-loco@mail.ru>
2021-03-23 02:51:43 +0300

    End crudV6

--- examples/crudV6/crudV6.go
+++ examples/crudV6/crudV6.go
@@ -14,6 +14,8 @@ import (
 	"time"
 )
 
+// Items -------------------------------------------------
+
 // Article struct
 type Article struct {
 	Id      string `json:"Id"`
@@ -22,40 +24,6 @@ type Article struct {
 	Content string `json:"Content"`
 }
 
-// ErrorMessage struct
-type ErrorMessage struct {
-	Message string `json:"Message"`
-}
-
-/* Local DataBase */
-var Articles = []Article{
-	{Id: "1", Title: "First title", Author: "First author", Content: "First content"},
-	{Id: "2", Title: "Second title", Author: "Second author", Content: "Second content"},
-}
-
-
-/* Postgres settings */
-type DbConf struct {
-	Port string
-	User string
-	Password string
-	DBname string
-	SSlmode string
-}
-
-var pgConf = DbConf{
-	Port: "54320",
-	User: "postgres",
-	Password: "2222",
-	DBname: "specgo",
-	SSlmode: "disable",
-}
-
-var connectStr = fmt.Sprintf("user=%v password=%v port= %v dbname=%v sslmode=%v",
-	pgConf.User, pgConf.Password, pgConf.Port, pgConf.DBname, pgConf.SSlmode)
-
-
-
 // User struct
 type User struct {
 	Id       int    `json:"id"`
@@ -63,25 +31,341 @@ type User struct {
 	Password string `json:"password"`
 }
 
+// ResponseMessage struct
+const successStatus string = "success"
+const errorStatus string = "error"
 
-/* Users Local DataBase */
-var Users = []User{
-	{1, "Bob", "2222"},
-	{2, "Alice", "1234"},
+type ResponseMessage struct {
+	Status  string      `json:"Status"`
+	Message interface{} `json:"Message"`
 }
+
+func (resMsg *ResponseMessage) setError(msg interface{}) {
+	resMsg.Status = errorStatus
+	resMsg.Message = msg
+}
+
+func (resMsg *ResponseMessage) setSuccess(msg interface{}) {
+	resMsg.Status = successStatus
+	resMsg.Message = msg
+}
+
+// Db settings struct
+type DbConf struct {
+	Port     string
+	User     string
+	Password string
+	DBname   string
+	SSlmode  string
+}
+
+// DB instance
+var DB *sql.DB
+
+// Postgres conf
+var pgConf = DbConf{
+	Port:     "54320",
+	User:     "postgres",
+	Password: "2222",
+	DBname:   "specgo",
+	SSlmode:  "disable",
+}
+
+var connectStr = fmt.Sprintf("user=%v password=%v port= %v dbname=%v sslmode=%v",
+	pgConf.User, pgConf.Password, pgConf.Port, pgConf.DBname, pgConf.SSlmode)
 
 // SecretKey
 var JWTSecretKey = []byte("secret")
 
+// --------------------------------------------------------
+// Functions
+// --------------------------------------------------------
 
-func Test(writer http.ResponseWriter, request *http.Request) {
-	db, err := sql.Open("postgres", connectStr)
+// Insert User
+func InsertUser(innerUser User) ResponseMessage {
+	var resMsg ResponseMessage
+	// Check Login exists
+	if userLoginExists(innerUser.Login) {
+		resMsg.setError("Login already exists.")
+		return resMsg
+	}
+	// Query
+	_, err := DB.Exec("INSERT INTO users (login, password) VALUES ($1, $2)", innerUser.Login, innerUser.Password)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
-	defer db.Close()
+	return resMsg
+}
 
-	rows, err := db.Query("SELECT * FROM users")
+// Check User login in DB
+func userLoginExists(innerLogin string) bool {
+	var count int
+	err := DB.QueryRow("SELECT count(id) FROM users WHERE login = $1", innerLogin).Scan(&count)
+	if err != nil {
+		panic(err)
+	}
+	if count > 0 {
+		return true
+	}
+	return false
+}
+
+// Check User Pass and Login in DB
+func userExists(innerUser User) bool {
+	var count int
+	err := DB.QueryRow("SELECT count(id) FROM users WHERE login = $1 AND password = $2",
+		innerUser.Login, innerUser.Password).Scan(&count)
+	if err != nil {
+		panic(err)
+	}
+	if count > 0 {
+		return true
+	}
+	return false
+}
+
+// --------------------------------------------------------
+// Actions
+// --------------------------------------------------------
+
+// Register user
+//
+// request: POST
+func Register(writer http.ResponseWriter, request *http.Request) {
+	reqBody, _ := ioutil.ReadAll(request.Body)
+	var innerUser User
+	json.Unmarshal(reqBody, &innerUser)
+	// set Header JSON
+	writer.Header().Set("Content-Type", "application/json")
+	// Insert
+	resMsg := InsertUser(innerUser)
+
+	if resMsg.Status == errorStatus {
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(resMsg)
+		return
+	}
+	resMsg.setSuccess("User registered.")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(resMsg)
+}
+
+// Get Token User
+//
+// request: POST
+func GetToken(writer http.ResponseWriter, requestPtr *http.Request) {
+	var innerUser User
+	var resMsg ResponseMessage
+	reqBody, _ := ioutil.ReadAll(requestPtr.Body)
+	json.Unmarshal(reqBody, &innerUser)
+	// set Header JSON
+	writer.Header().Set("Content-Type", "application/json")
+	if !userExists(innerUser) {
+		resMsg.setError("Password or login does not exist")
+		// set Header Status Unauthorized
+		writer.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(writer).Encode(resMsg)
+		return
+	}
+	// create Token
+	token := jwt.New(jwt.SigningMethodHS256)
+	// claims - нужен для конфигурации токена
+	claims := token.Claims.(jwt.MapClaims)
+	claims["admin"] = true
+	claims["name"] = "New User"
+	// Token lifetime
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	// получаем токен
+	tokenString, err := token.SignedString(JWTSecretKey)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	// send Token
+	resMsg.setSuccess(tokenString)
+	json.NewEncoder(writer).Encode(resMsg)
+}
+
+// token Validator (middleware)
+// Это распознаватель, который нам подтверждает, что используя наш SecretKey был создан токен.
+// То есть токен наш.
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return JWTSecretKey, nil
+	},
+})
+
+// Show all users
+//
+// request: GET
+func ShowUsers(writer http.ResponseWriter, requestPtr *http.Request) {
+	rows, err := DB.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	user := User{}
+	users := make([]User, 0)
+	for rows.Next() {
+		err := rows.Scan(&user.Id, &user.Login, &user.Password)
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal(err)
+			return
+		}
+		users = append(users, user)
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(users)
+}
+
+// Articles -----------------------------------------------
+
+// Show all articles
+//
+// request: GET
+func ShowArticles(writer http.ResponseWriter, requestPtr *http.Request) {
+	rows, err := DB.Query("SELECT * FROM articles")
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	article := Article{}
+	articles := make([]Article, 0)
+	for rows.Next() {
+		err := rows.Scan(&article.Id, &article.Title, &article.Author, &article.Content)
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal(err)
+			return
+		}
+		articles = append(articles, article)
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(articles)
+}
+
+// Show one article
+//
+// request: GET
+// param: url/{id}
+func ShowArticleById(writer http.ResponseWriter, requestPtr *http.Request) {
+	vars := mux.Vars(requestPtr)
+	row := DB.QueryRow("SELECT * FROM articles WHERE id = $1", vars["id"])
+	article := Article{}
+	err := row.Scan(&article.Id, &article.Title, &article.Author, &article.Content)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(article)
+}
+
+// Create article
+//
+// request: POST
+// param: raw json
+func CreateArticle(writer http.ResponseWriter, requestPtr *http.Request) {
+	reqBody, _ := ioutil.ReadAll(requestPtr.Body)
+	article := Article{}
+	json.Unmarshal(reqBody, &article)
+	/* add Article in DB */
+	err := DB.QueryRow("INSERT INTO articles (title, author, content) VALUES ($1, $2, $3) RETURNING id",
+		article.Title, article.Author, article.Content).Scan(&article.Id)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	var resMsg ResponseMessage
+	resMsg.setSuccess(article)
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(resMsg)
+}
+
+// Delete article
+//
+//request: DELETE
+//param: url/{id}
+func DeleteArticle(writer http.ResponseWriter, requestPtr *http.Request) {
+	vars := mux.Vars(requestPtr)
+	var resMsg ResponseMessage
+	result, err := DB.Exec("DELETE FROM articles WHERE id = $1", vars["id"])
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	if rows, _ := result.RowsAffected(); rows < 1 {
+		resMsg.setError("Not found article for delete with that ID")
+		writer.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(writer).Encode(resMsg)
+		return
+	}
+
+	resMsg.setSuccess("Article successfully deleted.")
+	json.NewEncoder(writer).Encode(resMsg)
+}
+
+// Update update
+//
+// request: PUT
+// param: url/{id}
+func UpdateArticle(writer http.ResponseWriter, requestPtr *http.Request) {
+	vars := mux.Vars(requestPtr)
+	reqBody, _ := ioutil.ReadAll(requestPtr.Body)
+	resMsg := ResponseMessage{}
+	innerArticle := Article{}
+
+	json.Unmarshal(reqBody, &innerArticle)
+	row := DB.QueryRow("SELECT * FROM articles WHERE id = $1", vars["id"])
+	dbArticle := Article{}
+	err := row.Scan(&dbArticle.Id, &dbArticle.Title, &dbArticle.Author, &dbArticle.Content)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	// update
+	if innerArticle.Content != "" {
+		dbArticle.Content = innerArticle.Content
+	}
+	if innerArticle.Author != "" {
+		dbArticle.Author = innerArticle.Author
+	}
+	if innerArticle.Title != "" {
+		dbArticle.Title = innerArticle.Title
+	}
+	_, err = DB.Exec(`UPDATE articles 
+								SET title = $1,
+								author = $2,
+								content = $3
+								WHERE id = $4`,
+		dbArticle.Title, dbArticle.Author, dbArticle.Content, dbArticle.Id)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	resMsg.setSuccess("Article successfully updated.")
+	json.NewEncoder(writer).Encode(resMsg)
+}
+
+// --------------------------------------------------------
+// Tests
+// --------------------------------------------------------
+
+// Custom test1
+func Test1(writer http.ResponseWriter, request *http.Request) {
+
+	rows, err := DB.Query("SELECT * FROM users")
 	if err != nil {
 		panic(err)
 	}
@@ -90,7 +374,7 @@ func Test(writer http.ResponseWriter, request *http.Request) {
 	for rows.Next() {
 		user := User{}
 		err := rows.Scan(&user.Id, &user.Login, &user.Password)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			continue
 		}
@@ -98,251 +382,29 @@ func Test(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+// Custom test2
+func Test2(writer http.ResponseWriter, request *http.Request) {
+	result := userLoginExists("Bob")
+	fmt.Println(result)
+}
 
-/* Insert User */
-func InsertUser(innerUser User) {
+// Custom test3
+func Test3(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func main() {
+
+	fmt.Println("REST API V2.0 worked....")
+
+	// open DB connect
 	db, err := sql.Open("postgres", connectStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	DB = db
 	defer db.Close()
 
-	_, err = db.Exec("INSERT INTO users (login, password) VALUES ($1, $2)",
-		innerUser.Login, innerUser.Password)
-	if err != nil {
-		fmt.Println(err)
-		log.Fatal(err)
-	}
-}
-
-
-/**
- * Register
- * request: POST
- */
-func Register (writer http.ResponseWriter, request *http.Request) {
-	reqBody, _ := ioutil.ReadAll(request.Body)
-	var innerUser User
-	json.Unmarshal(reqBody, &innerUser)
-	InsertUser(innerUser)
-
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("User registered."))
-}
-
-/**
- * Register
- * request: POST
- */
-/*func Register(writer http.ResponseWriter, request *http.Request) {
-	// данные пользователя
-	reqBody, _ :=  ioutil.ReadAll(request.Body)
-	var innerUser User
-	json.Unmarshal(reqBody, &innerUser)
-	innerUser.Id = 0
-	fmt.Println(innerUser)
-	// ищем имя пользователя и создаём id
-	for _, user := range Users {
-		if user.Login == innerUser.Login {
-			writer.WriteHeader(http.StatusBadRequest)
-			writer.Write([]byte("Login "+ innerUser.Login + "already exists, please change your Login."))
-			return
-		}
-		// создаём Id пользователя
-		if innerUser.Id < user.Id {
-			innerUser.Id = user.Id
-		}
-	}
-	innerUser.Id++
-	Users = append(Users, innerUser)
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("User registered."))
-}*/
-
-/**
- * Get Token
- * request: POST
- */
-func GetToken(writer http.ResponseWriter, requestPtr *http.Request) {
-	// получаем данные пользователя из запроса
-	reqBody, _ := ioutil.ReadAll(requestPtr.Body)
-	var innerUser User
-	// инициализируем пользователя
-	json.Unmarshal(reqBody, &innerUser)
-
-	// ищем пользователя
-	for _, user := range Users {
-		/*
-		 * если находим - передаём токен
-		 */
-		if innerUser.Login == user.Login && innerUser.Password == user.Password {
-			// создаём токен
-			token := jwt.New(jwt.SigningMethodHS256)
-			// claims - нужен для конфигурации токена
-			claims := token.Claims.(jwt.MapClaims)
-			claims["admin"] = true
-			claims["name"] = "New User"
-			// время существования токена
-			claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-			// получаем токен
-			tokenString, err := token.SignedString(JWTSecretKey)
-			if err != nil {
-				log.Fatal(err)
-			}
-			// отправляем пользователю
-			writer.Write([]byte(tokenString))
-			return
-		}
-	}
-
-	/*
-	 * Если не найден - отвечаем Unauthorize
-	 */
-	writer.WriteHeader(http.StatusUnauthorized)
-	writer.Write([]byte("User is not Found."))
-}
-
-
-/* token Validator (middleware)
- * Это распознаватель, который нам подтверждает, что используя наш SecretKey был создан токен.
- * То есть токен наш.
- */
-var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
-	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-		return JWTSecretKey, nil
-	},
-})
-
-
-
-/**
- * Show all
- * request: GET
- */
-func ShowUsers(writer http.ResponseWriter, requestPtr *http.Request) {
-	fmt.Println("Hint: ShowAllUsers worked...")
-	writer.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(writer).Encode(Users) // пишем в Response все Articles в виде JSON
-	/** TEST **/
-	//fmt.Println(Articles)
-}
-
-
-
-/**
- * Show all
- * request: GET
- */
-func ShowArticles(writer http.ResponseWriter, requestPtr *http.Request) {
-	fmt.Println("Hint: ShowAllArticles worked...")
-	writer.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(writer).Encode(Articles) // пишем в Response все Articles в виде JSON
-	/** TEST **/
-	//fmt.Println(Articles)
-}
-
-/**
- * Show one
- * request: GET
- * param: url/{id}
- */
-func ShowArticleById(writer http.ResponseWriter, requestPtr *http.Request) {
-	vars := mux.Vars(requestPtr)
-	find := false
-
-	for _, article := range Articles {
-		if article.Id == vars["id"] {
-			find = true
-			json.NewEncoder(writer).Encode(article)
-		}
-	}
-	if !find {
-		var err = ErrorMessage{Message: "Not found article with that ID"}
-		writer.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(writer).Encode(err)
-	}
-	/** TEST **/
-	//fmt.Println(Articles)
-}
-
-/**
- * Create
- * request: POST
- * param: raw json
- */
-func CreateArticle(writer http.ResponseWriter, requestPtr *http.Request) {
-	/*
-		{
-			"Id" : "3",
-			"Title" : "Title from json POST method",
-			"Author" : "Any Name",
-			"Content" : "Content from json POST method"
-		}
-	*/
-	reqBody, _ := ioutil.ReadAll(requestPtr.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	/* add Article in DB */
-	Articles = append(Articles, article)
-	/* return new Article */
-	writer.WriteHeader(http.StatusCreated)
-	json.NewEncoder(writer).Encode(article.Id)
-	/** TEST **/
-	//fmt.Println(Articles)
-}
-
-/**
- * Delete
- * request: DELETE
- * param: url/{id}
- */
-func DeleteArticle(writer http.ResponseWriter, requestPtr *http.Request) {
-	vars := mux.Vars(requestPtr)
-	find := false
-	for index, article := range Articles {
-		if article.Id == vars["id"] {
-			find = true
-			writer.WriteHeader(http.StatusAccepted)
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
-	if !find {
-		var err = ErrorMessage{Message: "Not found article for delete with that ID"}
-		writer.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(writer).Encode(err)
-	}
-}
-
-/**
- * Update
- * request: PUT
- * param: url/{id}
- */
-func UpdateArticle(writer http.ResponseWriter, requestPtr *http.Request) {
-	vars := mux.Vars(requestPtr)
-	idKey := vars["id"] // строка
-	find := false
-
-	for index, article := range Articles {
-		if article.Id == idKey {
-			find = true
-			reqBody, _ := ioutil.ReadAll(requestPtr.Body)
-			writer.WriteHeader(http.StatusAccepted)   // Изменяем статус код на 202
-			json.Unmarshal(reqBody, &Articles[index]) // перезаписываем всю информацию для статьи с Id
-		}
-	}
-
-	if !find {
-		writer.WriteHeader(http.StatusNotFound)
-		err := ErrorMessage{Message: "Not found article with that ID. Try use POST first"}
-		json.NewEncoder(writer).Encode(err)
-	}
-	/**** TEST ****/
-	fmt.Println(Articles)
-}
-
-func main() {
-	fmt.Println("REST API V2.0 worked....")
 	/* init mux Router */
 	router := mux.NewRouter().StrictSlash(true)
 
@@ -362,7 +424,6 @@ func main() {
 	router.HandleFunc("/article/{id}", ShowArticleById).Methods("GET")
 
 	/* create (with token) */
-	//router.HandleFunc("/article", CreateArticle).Methods("POST")
 	router.Handle("/article", jwtMiddleware.Handler(http.HandlerFunc(CreateArticle))).Methods("POST")
 
 	/* delete */
@@ -371,9 +432,10 @@ func main() {
 	/* update */
 	router.HandleFunc("/article/{id}", UpdateArticle).Methods("PUT")
 
-
-	/* test */
-	router.HandleFunc("/test", Test).Methods("GET")
+	/* tests */
+	router.HandleFunc("/test1", Test1).Methods("GET")
+	router.HandleFunc("/test2", Test2).Methods("GET")
+	router.HandleFunc("/test3", Test3).Methods("GET")
 
 	/* serve */
 	log.Fatal(http.ListenAndServe(":8050", router))
